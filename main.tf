eaf76b41def4ab5a55dbc84761876376ba060152
Vikas Bahuguna <vikasedu10@gmail.com>
2023-08-13 09:42:02 +0530

    Format tf code & move data block after eks module

--- main.tf
+++ main.tf
@@ -12,54 +12,47 @@ data "aws_availability_zones" "azs" {}
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = local.cluster_name
-  cidr = var.vpc_cidr_block
-  azs = data.aws_availability_zones.azs.names
+  name            = local.cluster_name
+  cidr            = var.vpc_cidr_block
+  azs             = data.aws_availability_zones.azs.names
   private_subnets = var.private_subnets_cidr
-  public_subnets = var.public_subnets_cidr
-  
+  public_subnets  = var.public_subnets_cidr
+
   enable_dns_hostnames = true
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 
   tags = merge(
     local.resource_tags, {
-      "kubernetes.io/cluster/${local.cluster_name}": "shared" 
+      "kubernetes.io/cluster/${local.cluster_name}" : "shared"
     }
   )
   private_subnet_tags = merge(
     local.resource_tags, {
-      "kubernetes.io/cluster/${local.cluster_name}": "shared" 
-      "kubernetes.io/role/internal-elb": 1
+      "kubernetes.io/cluster/${local.cluster_name}" : "shared"
+      "kubernetes.io/role/internal-elb" : 1
     }
   )
   public_subnet_tags = merge(
     local.resource_tags, {
-      "kubernetes.io/cluster/${local.cluster_name}": "shared" 
-      "kubernetes.io/role/elb": 1
+      "kubernetes.io/cluster/${local.cluster_name}" : "shared"
+      "kubernetes.io/role/elb" : 1
     }
   )
 }
-
-data "aws_eks_cluster" "cluster" {
-  name = local.cluster_name
-}
-data "aws_eks_cluster_auth" "cluster" {
-  name = local.cluster_name
-}
 provider "kubernetes" {
-  host = data.aws_eks_cluster.cluster.endpoint
-  token = data.aws_eks_cluster_auth.cluster.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
 }
 
 module "platform_eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  
+  source = "terraform-aws-modules/eks/aws"
+
   cluster_name    = local.cluster_name
   cluster_version = "1.27"
 
-  cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access = true
   cluster_addons = {
     aws-ebs-csi-driver = {
       most_recent = true
@@ -67,26 +60,27 @@ module "platform_eks" {
   }
 
   subnet_ids = module.vpc.private_subnets # If subnet IDs not provided, need to provide control_plane_subnet_ids
-  vpc_id = module.vpc.vpc_id
-  # eks_managed_node_groups = {
-    
-  #   min_node_count = var.min_node_count
-  #   max_node_count = var.max_node_count
-  #   instance_type = var.instance_type
-  # }
+  vpc_id     = module.vpc.vpc_id
+
   eks_managed_node_groups = {
     my_node_group = {
-      name = local.cluster_name
+      name           = local.cluster_name
       min_node_count = var.min_node_count
       max_node_count = var.max_node_count
-      instance_type = var.instance_type
-      tags = local.resource_tags
+      instance_type  = var.instance_type
+      tags           = local.resource_tags
     }
   }
 
-  iam_role_name = local.cluster_name
+  iam_role_name        = local.cluster_name
   iam_role_description = "IAM Role for EKS cluster created using Terraform IaC"
 
   tags = local.resource_tags # This module level tags will be added to all underlying resources
 
+}
+data "aws_eks_cluster" "cluster" {
+  name = local.cluster_name
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region                   = var.region
   shared_credentials_files = ["~/.aws/credentials"]
 }
 
@@ -32,19 +32,19 @@ variable "region" {
   default = "ap-south-1"
 }
 variable "private_subnets_cidr" {
-default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 variable "public_subnets_cidr" {
-default = ["10.0.101.0/24", "10.0.102.0/24"]
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 
 variable "instance_type" {
-default = "t3.medium"
+  default = "t3.medium"
 }
-variable "min_node_count"{
-default = "1"
+variable "min_node_count" {
+  default = "1"
 }
-variable "max_node_count"{
-default = "3"
+variable "max_node_count" {
+  default = "3"
 }
